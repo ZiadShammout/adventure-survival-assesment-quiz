1c44a840010eb0c1c73020f92afd2921c315e97f
Ziad Shammout <100541258+ZiadShammout@users.noreply.github.com>
2022-03-20 23:03:10 +0000

    I put spaces between sentences to make it more spaced out(not crowded)

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello and welcome to an adventure/survival quiiizzzzz!!!, in this quiz we will test your ability to think in desperate situations!!! here are some basic thing you should know before you start your quiz!for every question you get correct you will earn a point!!these points add up and can determine your level of intelligence!!how good you are at surviving!!! there are possibilaties where you can end this quiz in a shorter ammount of time!!this may happen because you pick the wrong option and end up un-survivinggg!!!when this happens you will be taken back to the start of the quiz!!enjoyyy!!!")
+print("""hello and welcome to an adventure/survival quiiizzz!, in this quiz we will test your ability to think in desperate situations!!! 
+      
+here are some basic things you should know before you start your quiz! 
+      
+for every question you get correct you will earn a point!!!these points add up and determine your level of intelligence!!how good are you are at surviving!!! 
+      
+there are possibilaties where you can end this quiz in a shorter ammount of time!!
+
+this may happen because you pick the wrong option and end up un-survivinggg! 
+      
+when this happens you will be taken back to the start of the quiz!!enjoyyy!!!""")
