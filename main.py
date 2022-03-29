59a201e104dd5703d8c45442e9dfd1d436211319
Ziad Shammout <100541258+ZiadShammout@users.noreply.github.com>
2022-03-29 20:49:21 +0000

    using functions for all different components

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("""Hello and welcome to an adventure/survival quiiizzz!, in this quiz we will test your ability to think in desperate situations!!! 
+def welcome():
+  print("""Hello and welcome to an adventure/survival quiiizzz!, in this quiz we will test your ability to think in desperate situations!!! 
 ***********************************************************************************
 Here are some basic things you should know before you start your quiz! 
 ***********************************************************************************
@@ -12,8 +13,9 @@ when this happens you will be taken back to the start of the quiz!!enjoyyy!!!
 ***********************************************************************************
 """)
 
-print("Please only enter name in letters")
-while True:
+def name():
+  print("Please only enter name in letters")
+  while True:
     try:
         name = input("please enter your name to begin: ")
         if name.isalpha():
@@ -21,10 +23,27 @@ while True:
     except ValueError:
         print("Please only enter name in letters")
       
-print("Hello " + name) 
+  print("Hello " + name) 
+  print("""lets begin!!
+        
+      """)
+
+score=0
+questions = { 'you wake up, take off your seat belt,stand up and see hundreds of people sleeping,what do you do? \n a: look around for anyone that is awake \n b: go back to sleep \n c: get out the aircraft and see the surroundings\n' : 'a' , 'Question 2\n a: answer a\n b: answer b\n c: answer c \n' :  'b' , 
+             'Question 3 \n a: answer a\n b: answer b\n c: answer c \n' : 'c' }
+
+def run_quiz():
+  for key in questions.keys():
+    user_answer=input(key).lower().strip()
+    if questions.get(key)==user_answer:
+        print("Correct!") 
+    else:
+        print("You're Wrong!")
 
 
-
-
-
+# Start of program
+welcome()
+name()
+run_quiz()
+ 
 
